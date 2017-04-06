0bdc798409c8c30e4b29d7a115268aba3914cd09
Merik <merik@voicetube.com>
2017-04-06 15:04:50 +0800

    Clean unnecessary requeriment & files

--- src/PaymentGatewayFacade.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace VoiceTube\TaiwanPaymentGateway;
-
-use Illuminate\Support\Facades\Facade;
-
-class PaymentGatewayFacade extends Facade{
-	protected static function getFacadeAccessor() { return 'PaymentGateway'; }
-}
